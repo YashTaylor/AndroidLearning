073ff5c69af2b2171874d14a006aa2d35543de14
YashTailor <yashtailor9287@gmail.com>
2026-02-01 22:22:20 +0530

    Live Data learning started

--- LiveData/app/src/main/java/com/example/livedata/MainActivity.kt
+++ LiveData/app/src/main/java/com/example/livedata/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.example.livedata.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    var counter = 0
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.button.setOnClickListener {
+            counter++
+            binding.textView2.text = counter.toString()
+        }
     }
 }
