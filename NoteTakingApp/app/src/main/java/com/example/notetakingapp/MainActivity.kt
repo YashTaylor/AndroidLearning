6666d5a13bd1f55b842c7689e3155eb44bf94a7d
yash <connect@adhyayinfotech.com>
2026-02-12 10:44:19 +0530

    Implemented mainActivity to connect everything

--- NoteTakingApp/app/src/main/java/com/example/notetakingapp/MainActivity.kt
+++ NoteTakingApp/app/src/main/java/com/example/notetakingapp/MainActivity.kt
@@ -5,17 +5,33 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.notetakingapp.database.NoteDatabase
+import com.example.notetakingapp.databinding.ActivityMainBinding
+import com.example.notetakingapp.repository.NoteRepository
 import com.example.notetakingapp.viewmodel.NoteViewModel
+import com.example.notetakingapp.viewmodel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity()
 {
-
     lateinit var noteViewModel: NoteViewModel
+
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupViewModel()
+    }
+
+    fun setupViewModel()
+    {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory).get(NoteViewModel::class.java)
     }
 }
