6e9acebcdb94fe477752cd81672eeeb1af6116b1
yash <connect@adhyayinfotech.com>
2026-02-17 11:22:26 +0530

    Sequential and parallel coroutines execution

--- CoroutinesApp/app/src/main/java/com/example/coroutinesapp/MainActivity.kt
+++ CoroutinesApp/app/src/main/java/com/example/coroutinesapp/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.databinding.DataBindingUtil
 import com.example.coroutinesapp.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -32,6 +34,36 @@ class MainActivity : AppCompatActivity()
             }
         }
 
+        // Sequential execution
+        CoroutineScope(Dispatchers.IO).launch {
+            val one = doSomething1()
+            val two = doSomething2()
+            val result = one + two
+            Log.i("TAGY", "onCreate, Result is: $result")
+        }
+
+        // Parallel execution using async
+        CoroutineScope(Dispatchers.IO).launch {
+            val one = async { doSomething1() }
+            val two = async { doSomething2() }
+            val result = one.await() + two.await()
+            Log.i("TAGY", "onCreate, Result is: $result")
+        }
+
+    }
+
+    suspend fun doSomething1(): Int
+    {
+        delay(3000)
+        Log.i("TAGY", "doSomething1: DONE")
+        return 11
+    }
+
+    suspend fun doSomething2(): Int
+    {
+        delay(5000)
+        Log.i("TAGY", "doSomething2: DONE")
+        return 22
     }
 
     private suspend fun downloadFile()
