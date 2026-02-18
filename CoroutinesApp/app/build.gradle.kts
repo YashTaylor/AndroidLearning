35248c60f9a34b4413cdba32a01ff6f7d2f8693f
yash <connect@adhyayinfotech.com>
2026-02-18 11:11:29 +0530

    cleanup build.gradle

--- CoroutinesApp/app/build.gradle.kts
+++ CoroutinesApp/app/build.gradle.kts
@@ -27,15 +27,6 @@ android {
             )
         }
     }
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
