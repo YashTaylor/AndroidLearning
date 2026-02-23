a94d1004d8948d7a9a6bc5ef463c6b559f3e230a
yash <connect@adhyayinfotech.com>
2026-02-23 14:04:30 +0530

    Implemented firebase authentication for user registration.

--- JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
+++ JournalApp/app/src/main/java/com/example/journalapp/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.example.journalapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity()
 {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.kt
+++ JournalApp/app/src/main/java/com/example/journalapp/SignUpActivity.kt
@@ -1,14 +1,69 @@
 package com.example.journalapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.journalapp.databinding.ActivitySignUpBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.auth
 
 class SignUpActivity : AppCompatActivity()
 {
+    lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up)
+
+        auth = Firebase.auth
+
+        binding.btnSignUp.setOnClickListener {
+            createUser()
+        }
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser != null)
+        {
+            reload()
+        }
+    }
+
+    private fun createUser()
+    {
+        val email = binding.tvEmail.text.toString()
+        val password = binding.etPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful)
+                {
+                    val user = auth.currentUser
+                    Toast.makeText(baseContext, "Authentication successful.", Toast.LENGTH_SHORT,).show()
+                    updateUI(user)
+                } else
+                {
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT,).show()
+                    updateUI(null)
+                }
+            }
+    }
+
+    private fun updateUI(user: FirebaseUser?)
+    {
 
     }
+
+    private fun reload()
+    {
+
+    }
+
 }
