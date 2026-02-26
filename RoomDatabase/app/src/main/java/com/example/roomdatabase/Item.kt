b323c58cb9102f52f9c301cb715b2eb8afaaa508
yash <connect@adhyayinfotech.com>
2026-02-26 10:23:50 +0530

    Room database implementation started

---
+++ RoomDatabase/app/src/main/java/com/example/roomdatabase/Item.kt
@@ -0,0 +1,13 @@
+package com.example.roomdatabase
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Item(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val price: Double,
+    val quantity: Int
+)
